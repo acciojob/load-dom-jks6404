732633a448578747468142cb2ca1aee9bd2df17b
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-06-18 20:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function() {
+      var body = document.querySelector('body');
+      body.textContent = 'DOM load success';
+    });
